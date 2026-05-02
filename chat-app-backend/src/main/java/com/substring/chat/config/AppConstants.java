3098545e3a94438f7ca5401097c312b9f7bc985c
Praveen Kumar <152891567+praveentiwari333@users.noreply.github.com>
2026-05-02 17:18:53 +0530

    Update FRONT_END_BASE_URL to production URL

--- chat-app-backend/src/main/java/com/substring/chat/config/AppConstants.java
+++ chat-app-backend/src/main/java/com/substring/chat/config/AppConstants.java
@@ -1,5 +1,5 @@
 package com.substring.chat.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL="http://localhost:5173";
+    public static final String FRONT_END_BASE_URL="https://chatfrontend-dvq2.onrender.com";
 }
